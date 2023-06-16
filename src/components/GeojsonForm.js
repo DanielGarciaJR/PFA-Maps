def1ccad86aa41801d4c9838a86fd70c65558b1c
Luis Daniel Garcia Guarneros <0319123808@ut-tijuana.edu.mx>
2023-06-16 14:02:11 -0700

    first push dev-evidence

---
+++ src/components/GeojsonForm.js
@@ -0,0 +1,28 @@
+import { useGeoJsonForm } from "@/hooks/usedGeojsonForm";
+import ErrorMessage from "./ErrorMessage";
+
+const GeojsonForm = () => {
+
+    const {handleChange,handleSubmit, error, fileRef,setError} = useGeoJsonForm();
+
+    return(
+        <div className='p-3'>
+            <div className='m-1'>
+                <h1 className="mb-3">Choose a GeoJson file</h1>
+                <form onSubmit={handleSubmit} className='text-[13px] flex flex-col'>
+                    <input type="file" accept=".geojson" required onChange={handleChange} onFocus={() => setError(false)} ref={fileRef}/>
+                    {error && 
+                        <ErrorMessage>
+                            <p className="bg-red-100 mt-2 p-2 text-red-600">
+                                Error: Only .geojson supported
+                            </p>
+                        </ErrorMessage>
+                    }
+                    <button type="submit" className="text-white bg-purple-700 w-20 p-1 rounded-md mt-5">Save</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default GeojsonForm;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,14 @@
-import { FaBuffer} from 'react-icons/fa';
+import { FaBuffer } from 'react-icons/fa';
+import { useSidebarMenu } from '@/hooks/useSidebarMenu';
 import LocationForm from './LocationForm';
 import TilesetContainer from './TilesetContainer';
+import GeojsonForm from './GeojsonForm';
+
 
 /*Component to show tileset Information*/
 const Sidebar = ({form,formSubmit,tilesets, newLocationInput}) => {
+
+    const { loadTileSection, loadUploadSection, section} = useSidebarMenu();
     
     return (
       <div className="bg-white w-[25%] h-screen  absolute top-0 right-0 drop-shadow-md overflow-y-scroll ">
@@ -17,28 +22,42 @@ const Sidebar = ({form,formSubmit,tilesets, newLocationInput}) => {
                 />
             </div>
 
-            {/*div for description*/}
-            <div className="mt-[10%] flex-col flex items-center justify-center">
-                <FaBuffer className="w-[30px] h-[30px] mb-[5%] "/>
-                <p 
-                  className="text-[20px] text-center">
-                  Tilesets Information
-                </p>
-                <p 
-                  className="text-center text-gray-400/70">
-                  Here you can find all tilesets affecting the address you wrote!
-                </p>
+            {/*div for content menu*/}
+            <div className="p-2 mt-4 text-[14px]">
+              <ul className="flex text-gray-500">
+                <li onClick={loadTileSection} className='m-2 hover:underline hover:cursor-pointer'>Tilesets</li>
+                <li onClick={loadUploadSection}  className='m-2 hover:underline hover:cursor-pointer'>Upload Files</li>
+              </ul>
             </div>
+            
+            {/*Tileset content*/}
+            {section.tile && 
+              <>
+                {/*div for description*/}
+                <div className="mt-[10%] flex-col flex items-center justify-center">
+                  <FaBuffer className="w-[30px] h-[30px] mb-[5%] "/>
+                  <p className="text-[20px] text-center">
+                    Tilesets Information
+                  </p>
+                  <p className="text-center text-gray-400/70">
+                    Here you can find all tilesets affecting the address you wrote!
+                  </p>
+                </div>
 
-            {/*div for tilesets affecting*/}
-            <div className=" flex  mt-5 flex-col">
-              {tilesets.length === 0 ? <div className='p-3 text-center mt-5'><p className=' text-gray-400/70'>No tilesets detected</p></div> : tilesets.map((el,index) => (
-                <TilesetContainer 
-                  key={index} 
-                  tile={el}>
-                </TilesetContainer>
-              ))}
-            </div>
+                {/*div for tilesets affecting*/}
+                <div className=" flex  mt-5 flex-col">
+                  {tilesets.length === 0 ? <div className='p-3 text-center mt-5'><p className=' text-gray-400/70'>No tilesets detected</p></div> : tilesets.map((el,index) => (
+                    <TilesetContainer 
+                      key={index} 
+                      tile={el}>
+                    </TilesetContainer>
+                ))}
+                </div>
+              </>
+            }
+
+            {/*Upload form content*/}
+            {section.upload && <GeojsonForm></GeojsonForm>}
         </div>
       </div>
     );
